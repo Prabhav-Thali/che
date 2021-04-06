8a71df1779c892fab07d6e42ccf5a5cc88e5a729
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-04-06 13:34:24 +0530

    Create build-native-debian.sh

---
+++ .github/build-native-debian.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -ex
+
+cd "$(dirname "$(dirname "$0")")"
+
+# Add stretch for Java 8
+cat <<END > /etc/apt/sources.list.d/stretch.list
+deb http://deb.debian.org/debian stretch main
+deb http://security.debian.org/debian-security stretch/updates main
+END
+
+#apt-get update -y
+#apt-get install -y --no-install-recommends openjdk-8-jdk-headless make gcc libc6-dev texinfo
+# Needs to be split, otherwise a newer version of OpenJDK is pulled
+#apt-get install -y --no-install-recommends ant
+#rm archive/*
+#ant jar && ant archive-platform-jar
+
+apt-get update -y && apt-get install -y ca-certificates-java openjdk-11-jdk wget curl  
+java -version
+wget https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.6.3/apache-maven-3.6.3-bin.tar.gz 
+tar -xzf apache-maven-3.6.3-bin.tar.gz  
+export PATH=`pwd`/apache-maven-3.6.3/bin/:$PATH 
+mvn -v
+mvn -B clean install -U -Pintegration	
