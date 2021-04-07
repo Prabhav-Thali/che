f8601e14ab025a350e8f8dbe9ae75d57d9ace516
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-04-07 11:04:30 +0530

    Update build-native-debian.sh

--- .github/build-native-debian.sh
+++ .github/build-native-debian.sh
@@ -5,10 +5,10 @@ set -ex
 cd "$(dirname "$(dirname "$0")")"
 
 # Add stretch for Java 8
-cat <<END > /etc/apt/sources.list.d/stretch.list
-deb http://deb.debian.org/debian stretch main
-deb http://security.debian.org/debian-security stretch/updates main
-END
+#cat <<END > /etc/apt/sources.list.d/stretch.list
+#deb http://deb.debian.org/debian stretch main
+#deb http://security.debian.org/debian-security stretch/updates main
+#END
 
 #apt-get update -y
 #apt-get install -y --no-install-recommends openjdk-8-jdk-headless make gcc libc6-dev texinfo
@@ -17,9 +17,7 @@ END
 #rm archive/*
 #ant jar && ant archive-platform-jar
 
-dpkg --force-depends --configure ca-certificates-java
-
-apt-get update -y && apt-get install -y openjdk-11-jdk wget curl  
+apt-get update -y && apt-get install -y openjdk-11-jdk=11.0.7+10-3ubuntu1 openjdk-11-jre=11.0.7+10-3ubuntu1 openjdk-11-jdk-headless=11.0.7+10-3ubuntu1 openjdk-11-jre-headless=11.0.7+10-3ubuntu1 wget curl
 java -version
 wget https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.6.3/apache-maven-3.6.3-bin.tar.gz 
 tar -xzf apache-maven-3.6.3-bin.tar.gz  
