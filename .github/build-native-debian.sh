2043ffb84bbb237aaef56599948e62bec017669d
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-04-07 11:19:21 +0530

    Update build-native-debian.sh

--- .github/build-native-debian.sh
+++ .github/build-native-debian.sh
@@ -17,6 +17,7 @@ cd "$(dirname "$(dirname "$0")")"
 #rm archive/*
 #ant jar && ant archive-platform-jar
 
+export DEBIAN_FRONTEND="noninteractive"
 apt-get update -y && apt-get install -y openjdk-11-jdk=11.0.7+10-3ubuntu1 openjdk-11-jre=11.0.7+10-3ubuntu1 openjdk-11-jdk-headless=11.0.7+10-3ubuntu1 openjdk-11-jre-headless=11.0.7+10-3ubuntu1 wget curl
 java -version
 wget https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.6.3/apache-maven-3.6.3-bin.tar.gz 
